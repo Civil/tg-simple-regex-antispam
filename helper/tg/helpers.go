2c90f7b7b06f6a5545e20814efc53c49b4db3682
Vladimir Smirnov <civiloid@google.com>
2024-09-11 20:24:56 +0200

    Make detailed log prettier

--- helper/tg/helpers.go
+++ helper/tg/helpers.go
@@ -21,6 +21,23 @@ func SendMessage(bot *telego.Bot, chatID telego.ChatID, messageID *int, text str
 	return err
 }
 
+func SendMarkdownMessage(bot *telego.Bot, chatID telego.ChatID, messageID *int, text string) error {
+	sendMessageParams := &telego.SendMessageParams{
+		ChatID:    chatID,
+		Text:      text,
+		ParseMode: telego.ModeMarkdownV2,
+	}
+	if messageID != nil {
+		sendMessageParams.ReplyParameters = &telego.ReplyParameters{
+			MessageID: *messageID,
+		}
+	}
+	_, err := bot.SendMessage(
+		sendMessageParams,
+	)
+	return err
+}
+
 func BanUser(bot *telego.Bot, chatID telego.ChatID, userID int64, deleteAll bool) error {
 	req := &telego.BanChatMemberParams{
 		UserID:         userID,
